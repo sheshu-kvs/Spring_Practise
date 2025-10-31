a6ae2477a8c334fd8e51b8417c3e5c52f70e8276
sheshu-kvs <kvs24161@gmail.com>
2025-10-31 15:36:02 +0530

    Spring Beans

--- springdemobeans/src/main/java/com/sheshu/springjdbc/car.java
+++ springdemobeans/src/main/java/com/sheshu/springjdbc/car.java
@@ -1,16 +1,19 @@
 package com.sheshu.springjdbc;
 
-import org.springframework.stereotype.Component;
 
 // @Component //this class is the bean class
-public class car {
- private final engine eng;
+    public class car {
+
+    private final engine eng;
 
     public car(engine eng){
     this.eng=eng;
     }
 
 
+
+
+    
     public String drive(){
         return eng.start()+" car is driving";
     }
--- springdemobeans/src/main/java/com/sheshu/springjdbc/engine.java
+++ springdemobeans/src/main/java/com/sheshu/springjdbc/engine.java
@@ -1,6 +1,5 @@
 package com.sheshu.springjdbc;
 
-import org.springframework.stereotype.Component;
 
 // @Component  // tells the this is the bean class
 public class engine{
@@ -8,5 +7,5 @@ public class engine{
     public String start(){
         return "Engine was started!!";
     }
-
+    
 }
